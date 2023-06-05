4f40bbda088f54b2de78120ab2e9067d7c38222f
dangminhtung <79414259+dangminhtung@users.noreply.github.com>
2023-06-06 01:17:53 +0700

    api

--- routers/cart_router.js
+++ routers/cart_router.js
@@ -3,7 +3,7 @@ var express = require("express")
 var cart_Controller = require('../controller/cartController')
 var router = express.Router();
 router.get('/list', cart_Controller.getCart)
-router.get('/add', cart_Controller.add);
+router.post('/add', cart_Controller.add);
 
 router.post('/deleteCart', cart_Controller.deleteToCart)
 module.exports = router;
