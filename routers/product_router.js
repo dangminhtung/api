b939dcea4cfccaa7ac5590323445c18b75ec28d1
dangminhtung <79414259+dangminhtung@users.noreply.github.com>
2023-06-05 23:04:40 +0700

    api

--- routers/product_router.js
+++ routers/product_router.js
@@ -17,37 +17,15 @@ const { arrangeAlphaAZ } = require("../models/product_model");
 // })
 
 // arrange Alpha A-Z
-router.get('/arrangeAlphaAZ', product_Controller.arrangeAlphaAZ)
-router.get('/arrangeAlphaAZ_cate/:cate', product_Controller.arrangeAlphaAZ_cate)
-// arrange Alpha Z-A
-router.get('/arrangeAlphaZA', product_Controller.arrangeAlphaZA)
-router.get('/arrangeAlphaZA_cate/:cate', product_Controller.arrangeAlphaZA_cate)
-// arrange Price low to high
-router.get('/arrangePriceAZ', product_Controller.arrangePriceAZ)
-router.get('/arrangePriceAZ_cate/:cate', product_Controller.arrangePriceAZ_cate)
-// arrange Price high to low
-router.get('/arrangePriceZA', product_Controller.arrangePriceZA)
-router.get('/arrangePriceZA_cate/:cate', product_Controller.arrangePriceZA_cate)
-// arrange createDate old to new 
-router.get('/arrangeDateAZ', product_Controller.arrangeDateAZ)
-router.get('/arrangeDateAZ_cate/:cate', product_Controller.arrangeDateAZ_cate)
-// arrange createDate new to old 
-router.get('/arrangeDateZA', product_Controller.arrangeDateZA)
-router.get('/arrangeDateZA_cate/:cate', product_Controller.arrangeDateZA_cate)
 
-
-
-router.get('/filterPrice', product_Controller.filterPrice)
-router.get('/filterFollowCate/:cate', product_Controller.filterPriceFollowCate)
-
-router.get('/test', product_Controller.test)
+router.get('/test/:id', product_Controller.getSize)
 
 router.get('/list', product_Controller.get_all_product);
 router.get('/sportShoe', product_Controller.get_shoe_sport);
 router.get('/gymShoe', product_Controller.get_shoe_gym)
 
-router.get('/:id', product_Controller.get_detail_product)
-router.get('/size_detail/:id', product_Controller.get_size_detail)
+
+router.get('/product_detail/:id', product_Controller.get_detail_product)
 router.post('/update', product_Controller.updateProduct)
 
 router.get('/recommend/:productID', product_Controller.getRecommend)
